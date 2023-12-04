62764d4de10a01bef9bdfc3dec92a969de06a10a
ahmedtaha-ux <ahmedtaha9792@gmail.com>
2023-12-04 23:03:07 +0200

    create user (model, controller ,router)

---
+++ models/users.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const userRoles = require('../utils/userRolels');
+const Schema = mongoose.Schema
+
+const user = new Schema ({
+    fullName: {type: String, required: true},
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    token : {type : String, required: false},
+    role: {type: String, enum: [userRoles.USER, userRoles.ADMIN, userRoles.MANGER], default: userRoles.USER}
+},
+{timestamps:true});
+
+const User = mongoose.model("User" , user)
+module.exports = User
