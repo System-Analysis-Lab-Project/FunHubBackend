85e36b6323b8fc0f971278d9ef60b7860ded65d6
ahmedtaha-ux <ahmedtaha9792@gmail.com>
2023-12-08 21:26:54 +0200

    Add search api

--- routers/api/product.router.js
+++ routers/api/product.router.js
@@ -1,10 +1,11 @@
-const productController = require('../../controllers/product.controller')
-const Router = require('express').Router
-const router = Router()
+const productController = require("../../controllers/product.controller");
+const Router = require("express").Router;
+const router = Router();
 
-router.get('/:_id', productController.get_product_by_id)
-router.get('/', productController.get_all_products)
-router.post('/', productController.add_product)
-router.put('/:_id', productController.update_product)
-router.delete('/:_id', productController.delete_product)
-module.exports = router
+router.get("/", productController.get_all_products);
+router.get("/:_id", productController.get_product_by_id);
+router.post('/search', productController.search_by_name);
+router.post("/", productController.add_product);
+router.put("/:_id", productController.update_product);
+router.delete("/:_id", productController.delete_product);
+module.exports = router;
